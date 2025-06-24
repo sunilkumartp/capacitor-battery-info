bbd22410dc15abad2d12993fd4aaab4be75940cb
Sunil Kumar <40438582+sunilkumartp@users.noreply.github.com>
2025-06-24 14:08:54 +0530

    Deleted

--- src/web.ts
+++ src/web.ts
@@ -16,7 +16,4 @@ export class BatteryInfoWeb extends WebPlugin implements BatteryInfoPlugin {
     };
   }
 
-  async echo(options: { value: string }): Promise<{ value: string }> {
-    return options;
-  }
 }
